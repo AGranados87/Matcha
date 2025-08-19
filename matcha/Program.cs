eab712fc04c4126a25fd92400590cea2964b06ae
ÁlvaroGR <agranadosruiz@hotmail.com>
2025-08-19 08:35:48 +0200

    Configurado acceso a base de datos básico.

--- matcha/Program.cs
+++ matcha/Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddControllers();
+
+
 
 var app = builder.Build();
 
