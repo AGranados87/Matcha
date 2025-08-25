77d2a51d03c6e1bc1dc070b33d07c0e4f496cc1a
ÁlvaroGR <agranadosruiz@hotmail.com>
2025-08-25 16:44:26 +0200

    Desarrollado CRUD de empleados.

---
+++ matcha/Modelo/Empleados.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace matcha.Modelo
+{
+    [Table("Empleados")]
+    public class Empleado
+    {
+        [Key]
+        public int EmpleadoID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string UserName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Salt { get; set; }
+
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+        [Required]
+        public bool Activo { get; set; } = true;
+    }
+}
