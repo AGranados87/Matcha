7847969de8fc9c0fb0a8fcfa9b175d3f76f2c4f9
ÁlvaroGR <agranadosruiz@hotmail.com>
2025-08-26 17:50:31 +0200

    Cambio en usuarios de ID a PacienteID, pacientes linkeados a psicologos.

--- matcha/Modelo/Usuarios.cs
+++ matcha/Modelo/Usuarios.cs
@@ -4,7 +4,7 @@ namespace matcha.Modelo
 {
     public class Usuario
     {
-        public int Id { get; set; }
+        public int PacienteID { get; set; }
 
         [Required(ErrorMessage = "El nombre es obligatorio")]
         public string Nombre { get; set; } = string.Empty;
@@ -12,5 +12,7 @@ namespace matcha.Modelo
         [Required(ErrorMessage = "El email es obligatorio")]
         [EmailAddress(ErrorMessage = "Formato de email inválido")]
         public string Email { get; set; } = string.Empty;
+        public int? EmpleadoID { get; set; }   // FK al psicólogo
+        public string Psicologo{ get; set; }  // Nombre del psicólogo
     }
 }
