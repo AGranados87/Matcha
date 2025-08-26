ceb253107450e74fd82f2a01c13c90c28b8382df
ÁlvaroGR <agranadosruiz@hotmail.com>
2025-08-26 16:39:05 +0200

    Introducción del calendario  en dashboard, creación modelo de citas.

---
+++ matcha/Modelo/Cita.cs
@@ -0,0 +1,11 @@
+﻿namespace matcha.Modelo
+{
+    public class Cita
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Paciente { get; set; }
+        public string Descripcion { get; set; }
+    }
+
+}
