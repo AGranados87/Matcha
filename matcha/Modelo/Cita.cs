7071edce07b8c9824511b1d4197f15faaf7f9cf6
ÁlvaroGR <agranadosruiz@hotmail.com>
2025-08-28 19:06:29 +0200

    Modificado modelo de Cita para posterior implementación.

--- matcha/Modelo/Cita.cs
+++ matcha/Modelo/Cita.cs
@@ -4,8 +4,9 @@
     {
         public int Id { get; set; }
         public DateTime Fecha { get; set; }
+        public TimeSpan Hora { get; set; }
         public string Paciente { get; set; }
         public string Descripcion { get; set; }
     }
-
 }
+
