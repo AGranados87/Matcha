6676dce43e32d9eaa3bab79ef616bbfb5c263d99
ÁlvaroGR <agranadosruiz@hotmail.com>
2025-09-04 19:06:30 +0200

    Separado model de Login

---
+++ matcha/Modelo/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace matcha.Modelo
+{
+    public class LoginModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
